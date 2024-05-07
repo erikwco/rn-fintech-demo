83eecf9877d9ebe22acb154bc44e8010726626ad
Erik Chacon <erikchacon@me.com>
2024-05-07 00:02:40 -0600

    home header and tab blur applied

---
+++ app/api/info+api.ts
@@ -0,0 +1,17 @@
+import { ExpoRequest, ExpoResponse } from 'expo-router/server';
+
+const API_KEY = process.env.CRYPTO_API_KEY;
+
+export async function GET(request: ExpoRequest) {
+  const ids = request.expoUrl.searchParams.get('ids');
+  const response = await fetch(
+    `https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id=${ids}`,
+    {
+      headers: {
+        'X-CMC_PRO_API_KEY': API_KEY!,
+      }
+    }
+  );
+  const result = await response.json();
+  return ExpoResponse.json(result.data)
+}
